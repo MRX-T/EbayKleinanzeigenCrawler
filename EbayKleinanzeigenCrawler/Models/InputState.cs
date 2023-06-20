3c1286c464a56f4d583dea8b43d12b54c891179a
Twix <40114067+MRX-T@users.noreply.github.com>
2023-06-20 18:03:06 +0200

    update file

---
+++ EbayKleinanzeigenCrawler/Models/InputState.cs
@@ -0,0 +1,14 @@
+namespace EbayKleinanzeigenCrawler.Models;
+
+public enum InputState
+{
+    Idle,
+    WaitingForUrl,
+    WaitingForIncludeKeywords,
+    WaitingForExcludeKeywords,
+    WaitingForInitialPull,
+    WaitingForTitle,
+    WaitingForSubscriptionToDelete,
+    WaitingForTitleToDisable,
+    WaitingForTitleToEnable
+}
