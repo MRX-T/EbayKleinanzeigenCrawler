9eea305a6fa20903bacbb58aea1ec08e24ac6167
Twix <40114067+MRX-T@users.noreply.github.com>
2023-06-20 18:03:13 +0200

    update file

---
+++ EbayKleinanzeigenCrawler/Persistence/SubscriptionPersistence.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using EbayKleinanzeigenCrawler.Interfaces;
+using EbayKleinanzeigenCrawler.Models;
+using Serilog;
+
+namespace EbayKleinanzeigenCrawler.Persistence;
+
+public class SubscriptionPersistence : ISubscriptionPersistence
+{
+    private readonly ILogger _logger;
+    protected readonly IDataStorage DataStorage;
+    protected ConcurrentBag<Subscriber> SubscriberList { get; set; }
+
+    public SubscriptionPersistence(ILogger logger, IDataStorage dataStorage)
+    {
+        Directory.CreateDirectory("data");
+        _logger = logger;
+        DataStorage = dataStorage;
+        SubscriberList = new ConcurrentBag<Subscriber>();
+        RestoreData();
+    }
+
+    public void AddSubscriber(Subscriber subscriber)
+    {
+        SubscriberList.Add(subscriber);
+        SaveData();
+    }
+
+    public Subscriber[] GetSubscribers()
+    {
+        return SubscriberList.ToArray();
+    }
+
+    public bool RestoreData()
+    {
+        try
+        {
+            DataStorage.Load(Path.Join("data", "Subscribers.json"), out ConcurrentBag<Subscriber> data);
+            SubscriberList = data;
+            _logger.Information($"Restored data: {data.Count} Subscribers");
+        }
+        catch (Exception e)
+        {
+            SubscriberList ??= new ConcurrentBag<Subscriber>();
+
+            if (e is FileNotFoundException)
+            {
+                _logger.Warning($"Could not restore subscribers: {e.Message}");
+            }
+            else
+            {
+                _logger.Error(e, $"Error when restoring subscribers: {e.Message}");
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public void SaveData()
+    {
+        DataStorage.Save(SubscriberList, Path.Join("data", "Subscribers.json"));
+    }
+
+    public List<Subscription> GetEnabledSubscriptions()
+    {
+        return GetSubscribers()
+            .SelectMany(s => s.Subscriptions)
+            .Where(s => s.Enabled)
+            .ToList();
+    }
+}
