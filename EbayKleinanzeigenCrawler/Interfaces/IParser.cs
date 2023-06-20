1bdbf66d97275c94a31578957e2cdd46c6120e21
Twix <40114067+MRX-T@users.noreply.github.com>
2023-06-20 18:03:01 +0200

    update file

---
+++ EbayKleinanzeigenCrawler/Interfaces/IParser.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using EbayKleinanzeigenCrawler.Models;
+using HtmlAgilityPack;
+
+namespace EbayKleinanzeigenCrawler.Interfaces;
+
+public interface IParser
+{
+    IQueryExecutor GetQueryExecutor();
+    List<Uri> GetAdditionalPages(HtmlDocument document);
+    IEnumerable<Result> ParseLinks(HtmlDocument document);
+    bool IsMatch(HtmlDocument document, Subscription subscription);
+}
